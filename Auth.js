ad3b8c164912908067a5606a5a4a15ebf3a7c7d6
ozo-vehe <johnnie.vehe@gmail.com>
2023-03-23 22:55:34 +0100

    edited the auth.js file

--- Auth.js
+++ Auth.js
@@ -1,6 +1,6 @@
 const authBtn  = document.querySelector(".auth-btn");
-const CLIENT_ID = "a787e9c6fba0418ba289ea9a1831dd8a";
-const REDIRECT_URL = "https://spotify-playlist-gen-v2.vercel.app/#";
+const CLIENT_ID = "89e5a59d75d74c3a98a357a8fcc664cb";
+const REDIRECT_URL = "https://spotify-playlist-gen-v2.vercel.app/";
 const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
 const RESPONSE_TYPE = "token"
 const SCOPES = [
@@ -18,5 +18,5 @@ const SCOPES_URL = SCOPES.join(SCOPES_DEL);
 
 authBtn.addEventListener("click", async function(e) {
   e.preventDefault();
-  window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPES_URL}&response_type=token&show_dialog=true`;
+  window.location.href = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL}&scope=${SCOPES_URL}&response_type=${RESPONSE_TYPE}&show_dialog=true`;
 })
